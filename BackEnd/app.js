e78a8aa4904302d0b942b11df36d780d9a502542
HacKairos <joao.brasilpv@gmail.com>
2022-09-03 02:36:17 -0300

    Reorganização do repositorio para o aplicativo

---
+++ BackEnd/app.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+})
+
+app.listen(3001, ()=>{
+    console.log("Server is Listening")
+})
