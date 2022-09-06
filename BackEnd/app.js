730540b47c88e6d60436327c6e241973aa1d0ef4
hannanhunny01 <hannanhoney5000@gmail.com>
2022-09-05 22:40:03 -0300

    registrartion api

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -10,10 +10,42 @@ const app = express();
 dotenv.config({path: './config.env'});
 require('./db/conn');
 
+//require model
+const Users = require('./models/userSchema');
+
 
 app.get('/', (req, res)=>{
     res.send("Hello World");
 })
+//Registration
+app.post('./register',(req,res)=>{ 
+    try{
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+        const phone = req.body.phone;
+
+        const createUser = new Users({
+            username : username,
+            email: email,
+            password:password,
+            phone:phone
+
+        });
+        //save method or insert
+        const created = await createUser.save();
+        console.log(created);
+        res.status(200).send("registrado com sucesso");
+    }
+    catch(error){
+        re.status(400).send(error)
+    }
+
+        
+    
+
+})
+
 
 app.listen(3001, ()=>{
     console.log("Server is Listening")
